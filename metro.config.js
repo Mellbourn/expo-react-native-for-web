405b01ceba1681eff3fdaf1e1ec4f32274ef2339
Klas Mellbourn <klas@mellbourn.net>
2024-03-08 14:36:42 +0100

    clean up

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const path = require("path");
-const fs = require("fs");
 const { getDefaultConfig } = require("expo/metro-config");
 const customer = process.env.CUSTOMER;
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
-// console.log("🚀 ~ config:", config);
 
-const extendedConfig = {
+module.exports = {
   ...config,
   resolver: {
     ...config.resolver,
@@ -17,35 +14,5 @@ const extendedConfig = {
         config.resolver.sourceExts
       ),
     ],
-    // resolveRequest: (context, realModuleName, platform, moduleName) => {
-    //   //   console.log("🚀 ~ context:", context);
-    //   //   console.log("🚀 ~ realModuleName:", realModuleName);
-    //   //   console.log("🚀 ~ platform:", platform);
-    //   //   console.log("🚀 ~ moduleName:", moduleName);
-    //   const baseName = path.resolve(context.originModulePath, realModuleName);
-    //   //   console.log("🚀 ~ baseName:", baseName);
-    //   const customerSpecificFile = `${baseName}.${customer}.ts`;
-
-    //   //   console.log("🚀 ~ customerSpecificFile:", customerSpecificFile);
-    //   //   if (fs.existsSync(customerSpecificFile)) {
-    //   //     console.log(
-    //   //       "********************************************************************************************FOUND",
-    //   //       customerSpecificFile
-    //   //     );
-    //   //     return {
-    //   //       filePath: customerSpecificFile,
-    //   //       type: "sourceFile",
-    //   //     };
-    //   //   }
-
-    //   // Fallback to Metro's default resolver if no customer-specific file is found
-    //   const result = context.resolveRequest(context, realModuleName, platform);
-    //   //   console.log("🚀 ~ result:", result);
-    //   return result;
-    // },
   },
 };
-// console.log("🚀 ~ qextendedConfig:", extendedConfig);
-console.log("extendedConfig", extendedConfig.resolver.sourceExts);
-
-module.exports = extendedConfig;
