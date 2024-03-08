3ecd9ddf26a57db8129d90058845f80dceb3af36
Klas Mellbourn <klas@mellbourn.net>
2024-03-08 11:21:08 +0100

    add ios specific file

---
+++ Suffix.ios.tsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+import { Button, Text } from "react-native";
+
+export const Suffix = memo(function suffix({
+  options: { subComponentState: num },
+  handleClick,
+}: {
+  options: {
+    subComponentState: number;
+  };
+  handleClick: () => void;
+}) {
+  console.log("Suffix rendered");
+  return (
+    <>
+      <Button onPress={handleClick} title="SubComponent state ios specific" />
+      <Text>Number: {num}</Text>
+    </>
+  );
+});
